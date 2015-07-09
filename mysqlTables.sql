e560a11f4fbb1036ef5a014d40b5d29e530f17d8
Edgar Doiron <edgar.doiron@gmail.com>
2015-07-09 00:32:22 -0400

    data restructure

--- mysqlTables.sql
+++ mysqlTables.sql
@@ -2,23 +2,23 @@ CREATE TABLE `guilds` (
   `guild_id` varchar(255) NOT NULL,
   `guild_name` varchar(255) NOT NULL,
   `tag` varchar(10) NOT NULL,
-  `background_id` int(11) DEFAULT NULL,
-  `foreground_id` int(11) DEFAULT NULL,
   `flag_id` int(11) unsigned NOT NULL,
-  `background_color_id` int(11) DEFAULT NULL,
-  `foreground_primary_color_id` int(11) DEFAULT NULL,
-  `foreground_secondary_color_id` int(11) DEFAULT NULL,
   `last_seen` datetime DEFAULT NULL,
   PRIMARY KEY (`guild_id`),
-  UNIQUE KEY `flags_UNIQUE` (`flags`)
-) ENGINE=InnoDB AUTO_INCREMENT=1748 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `flags_UNIQUE` (`flag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 --
 CREATE TABLE `guild_emblem_flags` (
   `id` int(11) NOT NULL,
-  `FlipBackgroundHorizontal` bit(1) DEFAULT b'0',
-  `FlipBackgroundVertical` bit(1) DEFAULT b'0',
-  `FlipForegroundHorizontal` bit(1) DEFAULT b'0',
-  `FlipForegroundVertical` bit(1) DEFAULT b'0',
+  `FlipBackgroundHorizontal` int(1) DEFAULT NULL,
+  `FlipBackgroundVertical` int(1) DEFAULT NULL,
+  `FlipForegroundHorizontal` int(1) DEFAULT NULL,
+  `FlipForegroundVertical` int(1) DEFAULT NULL,
+  `background_id` int(11) DEFAULT NULL,
+  `foreground_id` int(11) DEFAULT NULL,
+  `background_color_id` int(11) DEFAULT NULL,
+  `foreground_primary_color_id` int(11) DEFAULT NULL,
+  `foreground_secondary_color_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 --
